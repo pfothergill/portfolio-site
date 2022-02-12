db03b206e35777e299ce6abcbaedcfd354331920
Paul Fothergill <pfothergill@hotmail.com>
2022-02-12 13:53:21 -0600

    Adding GitHub actions

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import { Typography } from "@material-ui/core";
 
 function Contact() {
-  return (
-    <Typography>
-      <div className="container">
-        <h1>About me</h1>
-      </div>
-    </Typography>
-  );
+    return (
+        <Typography>
+            <div className="container">
+                <h1>About me</h1>
+                <p>I'm a developer for Principal Financial Group</p>
+                <p>I like to play golf!</p>
+            </div>
+        </Typography>
+    );
 }
 export default Contact;
