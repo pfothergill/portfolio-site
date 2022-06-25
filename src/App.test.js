ac3e58b116f6890ac87a08e36f72719ecee0b611
Paul Fothergill <pfothergill@hotmail.com>
2022-06-25 12:26:23 -0500

    Fixing failing test - updated for no contact link

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,8 @@ test("renders learn react link", () => {
     render(<App />);
 
     const aboutNavLink = screen.getByText(/About/i);
-    const contactNavLink = screen.getByText(/Contact/i);
     const faqNavLink = screen.getByText(/Faq/i);
 
     expect(aboutNavLink).toBeInTheDocument();
-    expect(contactNavLink).toBeInTheDocument();
     expect(faqNavLink).toBeInTheDocument();
 });
