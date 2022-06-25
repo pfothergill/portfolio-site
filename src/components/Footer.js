1fa5ecba658218c6a1a776402014168e50859ba2
Paul Fothergill <pfothergill@hotmail.com>
2022-06-25 12:23:54 -0500

    fixing display errors with dark-mode to light-mode

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/style.scss";
+
+function Footer() {
+    // const theme = useTheme();
+    // const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+    return (
+        <div className="row footer">
+            <b>PFOTHERGILL.COM ©2022</b>
+        </div>
+    );
+}
+export default Footer;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -41,9 +41,6 @@ function Navbar() {
                                 <Link className="nav-item" to="/about">
                                     About
                                 </Link>
-                                <Link className="nav-item" to="/contact">
-                                    Contact
-                                </Link>
                                 <Link className="nav-item" to="/faq">
                                     FAQ
                                 </Link>
