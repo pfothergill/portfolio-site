651fd9f981999e3acb44d492c45f8a75dacb3c52
Paul Fothergill <pfothergill@hotmail.com>
2026-05-02 10:42:49 -0500

    feat: trigger deploy

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,7 @@ function Home() {
     return (
         <div class="row content homepage">
             <div>
-                <h1>Under construction</h1>
+                <h1>Under construction!</h1>
                 <p>Please check back soon!</p>
             </div>
         </div>
